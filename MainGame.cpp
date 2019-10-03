4274be9b4c15f74f358123466fdc279458c6c9df
Francisco Perez <perezfg@vcu.edu>
2019-10-03 18:32:32 -0400

    NEW AND IMPROVED!!! NO HARD CODE

--- MainGame.cpp
+++ MainGame.cpp
@@ -10,23 +10,25 @@ int main() {
 	int x, y;
 	int points = 0;
 	do {
-		char realGameBoard[8][8] = {' '};
+		char realGameBoard[SIZE][SIZE] = {' '};
+		int countX = 1;
+		int countY = 1;
 		int guess = 5;
 		hiddenGameBoard();
 		ruleBoard();
 		srand(time(NULL));
-		int goldCount=0;
-		int bombCount=0;
-		while (goldCount <5) {
-			int gold1 = (rand() % 7);
-			int gold2 = (rand() % 7);
+		int goldCount = 0;
+		int bombCount = 0;
+		while (goldCount < 5) {
+			int gold1 = (rand() % (SIZE-2))+2;
+			int gold2 = (rand() % (SIZE-2))+2;
 			realGameBoard[gold1][gold2] = 'G';
 			goldCount++;
 		}
 		cout << endl;
 		while (bombCount < 1) {
-			int bomb1 = (rand() % 7);
-			int bomb2 = (rand() % 7);
+			int bomb1 = (rand() % (SIZE-2))+2;
+			int bomb2 = (rand() % (SIZE-2))+2;
 			realGameBoard[bomb1][bomb2] = 'B';
 			bombCount++;
 		}
@@ -35,10 +37,10 @@ int main() {
 			cin >> x;
 			cout << "Enter y coordinates: " << endl;
 			cin >> y;
-			x--;
-			y--;
+			x++;
+			y++;
 			if (realGameBoard[x][y] == 'G') {
-				cout << "You found GOLD!! Only " << ++guess << " guesses to go! You have "<< ++points << " points!"<< endl << endl;
+				cout << "You found GOLD!! Only " << ++guess << " guesses to go! You have " << ++points << " points!" << endl << endl;
 				realGameBoard[x][y] = 'F';
 			}
 			else if (realGameBoard[x][y] == 'B') {
@@ -52,20 +54,46 @@ int main() {
 			if (guess == 0) {
 				cout << "You earned " << points << " points" << endl;
 			}
-		} while (bomb == 0 && guess !=0);
+		} while (bomb == 0 && guess != 0);
 		cout << "You earned " << points << " points" << endl;
 		cout << "Better Luck Next Time" << endl;
 		cout << "Here's your board" << endl;
-		cout << "    1 2 3 4 5 6 7 8" << endl;
-		cout << "  _ _ _ _ _ _ _ _ _" << endl;
-		cout << "1 | " << realGameBoard[0][0] << " " << realGameBoard[1][0] << " " << realGameBoard[2][0] << " " << realGameBoard[3][0] << " " << realGameBoard[4][0] << " " << realGameBoard[5][0] << " " << realGameBoard[6][0] << " " << realGameBoard[7][0] << endl;
-		cout << "2 | " << realGameBoard[0][1] << " " << realGameBoard[1][1] << " " << realGameBoard[2][1] << " " << realGameBoard[3][1] << " " << realGameBoard[4][1] << " " << realGameBoard[5][1] << " " << realGameBoard[6][1] << " " << realGameBoard[7][1] << endl;
-		cout << "3 | " << realGameBoard[0][2] << " " << realGameBoard[1][2] << " " << realGameBoard[2][2] << " " << realGameBoard[3][2] << " " << realGameBoard[4][2] << " " << realGameBoard[5][2] << " " << realGameBoard[6][2] << " " << realGameBoard[7][2] << endl;
-		cout << "4 | " << realGameBoard[0][3] << " " << realGameBoard[1][3] << " " << realGameBoard[2][3] << " " << realGameBoard[3][3] << " " << realGameBoard[4][3] << " " << realGameBoard[5][3] << " " << realGameBoard[6][3] << " " << realGameBoard[7][3] << endl;
-		cout << "5 | " << realGameBoard[0][4] << " " << realGameBoard[1][4] << " " << realGameBoard[2][4] << " " << realGameBoard[3][4] << " " << realGameBoard[4][4] << " " << realGameBoard[5][4] << " " << realGameBoard[6][4] << " " << realGameBoard[7][4] << endl;
-		cout << "6 | " << realGameBoard[0][5] << " " << realGameBoard[1][5] << " " << realGameBoard[2][5] << " " << realGameBoard[3][5] << " " << realGameBoard[4][5] << " " << realGameBoard[5][5] << " " << realGameBoard[6][5] << " " << realGameBoard[7][5] << endl;
-		cout << "7 | " << realGameBoard[0][6] << " " << realGameBoard[1][6] << " " << realGameBoard[2][6] << " " << realGameBoard[3][6] << " " << realGameBoard[4][6] << " " << realGameBoard[5][6] << " " << realGameBoard[6][6] << " " << realGameBoard[7][6] << endl;
-		cout << "8 | " << realGameBoard[0][7] << " " << realGameBoard[1][7] << " " << realGameBoard[2][7] << " " << realGameBoard[3][7] << " " << realGameBoard[4][7] << " " << realGameBoard[5][7] << " " << realGameBoard[6][7] << " " << realGameBoard[7][7] << endl;
+		for (int y = 0; y < SIZE; y++) {
+			for (int x = 0; x < SIZE; x++) {
+				if (x == 1 && y == 1) {
+					cout << "_";
+				}
+				else if (x <= 1 && y <= 1) {
+					cout << " ";
+				}
+				else if (x <= SIZE && y == 1) {
+					cout << "_";
+				}
+				else if (y <= SIZE && x == 1) {
+					cout << "|";
+				}
+				else if (x <= SIZE && y == 0) {
+					cout << countX++;
+				}
+				else if (y <= SIZE && x == 0) {
+					cout << countY++;
+				}
+				else if (realGameBoard[x][y] == 'F') {
+					cout << "F";
+				}
+				else if (realGameBoard[x][y] == 'G') {
+					cout << "G";
+				}
+				else if (realGameBoard[x][y] == 'B') {
+					cout << "B";
+				}
+				else {
+					cout << " ";
+				}
+				cout << " ";
+			}
+			cout << endl;
+		}
 		cout << "Play again? Enter 'Y'" << endl;
 		cin >> choice;
 
@@ -77,23 +105,37 @@ int main() {
 }
 
 void hiddenGameBoard() {
-	char hiddenArray[SIZE][SIZE] = {
-		{' ', ' ', '1', '2','3', '4', '5', '6', '7', '8'},
-		{' ', '_', '_', '_','_', '_', '_', '_', '_', '_'},
-		{'1', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'2', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'3', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'4', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'5', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'6', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'7', '|', '?', '?','?', '?', '?', '?', '?', '?'},
-		{'8', '|', '?', '?','?', '?', '?', '?', '?', '?'} };
-	for (int x = 0; x < SIZE; x++) {
-		for (int y = 0; y < SIZE; y++) {
-			cout << hiddenArray[x][y]<< ' ';
+	int countX = 1;
+	int countY = 1;
+	char hiddenArray[SIZE][SIZE] = { };
+	for (int y = 0; y < SIZE; y++) {
+		for (int x = 0; x < SIZE; x++) {
+			if (x == 1 && y == 1) {
+				cout << "_";
+			}
+			else if (x <= 1 && y <= 1) {
+				cout << " ";
+			}
+			else if (x <= SIZE && y == 1) {
+				cout << "_";
+			}
+			else if (y <= SIZE && x == 1) {
+				cout << "|";
+			}
+			else if (x <= SIZE && y == 0) {
+				cout << countX++;
+			}
+			else if (y <= SIZE && x == 0) {
+				cout << countY++;
+			}
+			else {
+				cout << "?";
+			}
+			cout << " ";
 		}
 		cout << endl;
 	}
+
 }
 void ruleBoard() {
 	cout << "   __________________" << endl;
